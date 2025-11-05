9e1e47faf3cf49c6485079c5a3748a6d2c6e5893
Jag H. <jaggz.h@gmail.com>
2025-11-04 16:43:01 -0800

    added missing flags.h to server-tcp

---
+++ server-tcp/flags.h
@@ -0,0 +1,7 @@
+/******************
+/* Debug options */
+// Show data as it comes in
+#define DEBUG_FLAG_SHOW_DATA 0x01
+// If showing data, show ALL data (not truncated)
+#define DEBUG_FLAG_ALL_DATA  0x02
+
